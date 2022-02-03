cba4178757f81e082cdc57f68c0a214aa15bb528
dunnoconnor <michael.dunn.oconnor@gmail.com>
2022-02-03 14:55:04 -0500

    full crud

--- saucy-site/src/components/Header.jsx
+++ saucy-site/src/components/Header.jsx
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 function Header(props) {
     return (
         <div>
-           <Link to='/'><h3>Sauces</h3></Link>
+           <Link className='link' to='/'><h3>View Sauces</h3></Link>
+           <Link className='link'to='/new'><h3>Add New Sauce</h3></Link>
            {props.sauces.map((sauce) => {
-               return <Link key={sauce.id} to={`/sauces/${sauce.id}`}>{sauce.name}</Link>
+               return <Link className='link'key={sauce.id} to={`/sauces/${sauce.id}`}>{sauce.name}</Link>
            })}
         </div>
     );
